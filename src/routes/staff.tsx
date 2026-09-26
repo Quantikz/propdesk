966dfbbbdfcc785b18ca62f8ab58e0dd96a89dff
Quantikz <77287598+Quantikz@users.noreply.github.com>
2026-09-26 21:31:54 +0100

    Remove staff route

--- src/routes/staff.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { AppShell } from "@/components/propdesk/app-shell";
-
-export const Route = createFileRoute("/staff")({ component: Page });
-
-function Page() {
-  return <AppShell />;
-}
